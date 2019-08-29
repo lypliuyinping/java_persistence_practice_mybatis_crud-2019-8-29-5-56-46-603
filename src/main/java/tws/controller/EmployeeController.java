e5b85a642f3e71d9d67aa24bda92d164642f9123
liuyinping <1689372042@qq.com>
2019-08-29 21:15:45 +0800

    modify

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
@@ -25,5 +30,32 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
+    
+    @PostMapping
+	 @ResponseStatus(HttpStatus.CREATED)
+	 public void addEmployee(@RequestBody Employee employee) {
+		 employeeMapper.insertEmpployee(employee);
+	 }
+    
+//    @GetMapping
+//	 @ResponseStatus(HttpStatus.OK)
+//	 public List<Employee> getEmployee() {
+//		 List<Employee> employee = employeeMapper.selectAll();
+//		 return employee;
+//	 }	 	 
+	 
+	 @PutMapping("/{id}")
+	 @ResponseStatus(HttpStatus.OK)
+	 public void updateEmployee(@RequestBody Employee employee) {	
+		  employeeMapper.updateEmpployees(employee);
+			 
+		
+	 }
+
+	 @DeleteMapping("/{id}")
+	 @ResponseStatus(HttpStatus.OK)
+	 public void deleteEmployee(@PathVariable int id) {	
+		 employeeMapper.deleteEmpployees(id);
+	 }	
 
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +10,12 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+   // List<Employee> getEmployees();
+    void insertEmpployee(@Param(value = "employee") Employee employee);
+    
+    void updateEmpployees(@Param(value = "employee") Employee employee);
+    
+    void deleteEmpployees(@Param(value = "id") int id);
+    
+    
 }
